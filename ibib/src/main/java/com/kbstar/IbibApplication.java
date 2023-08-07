f3a873145c4c48be4552496fd9fbf65a8fe1a458
jiyoung0123 <taebin100@gmail.com>
2023-08-07 15:25:53 +0900

    1

---
+++ ibib/src/main/java/com/kbstar/IbibApplication.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbibApplication.class, args);
+	}
+
+}
---
+++ ibib/src/main/java/com/kbstar/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.kbstar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+
+@Controller
+public class MainController {
+	@RequestMapping
+	public String main() {
+		return "login";
+	}
+	
+	
+
+}
